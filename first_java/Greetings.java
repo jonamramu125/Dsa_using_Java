582de438d8384c5e3c684fdf8dae0e69e3a6c3ca
jonamramu125 <162277592+jonamramu125@users.noreply.github.com>
2024-09-18 23:01:06 +0530

    Update Greetings.java

--- first_java/Greetings.java
+++ first_java/Greetings.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the name:");
+        String name = input.nextLine();
+        System.out.println("Hello motherFucker "+ name);
+        input.close();
+    }
+}
 
